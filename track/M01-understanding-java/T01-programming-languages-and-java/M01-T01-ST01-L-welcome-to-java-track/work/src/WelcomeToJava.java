fc9907c33f2c92dcd897645aec54ab29c8c10afc
Yallanjaneyulu <yallanjaneyulu@gmail.com>
2026-07-30 13:08:42 +0530

    Day1change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,5 +5,6 @@ public class WelcomeToJava {
         System.out.println("welcome to java Track");
         System.out.println("I already know how to build programming logic");
         System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("Now I know how to use fit and github");
     }
 }
