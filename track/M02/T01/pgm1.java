df67f59c36f050a1f1d7754f6039fee2cdfe7717
Yallanjaneyulu <yallanjaneyulu@gmail.com>
2026-08-06 11:45:49 +0530

    neted

---
+++ track/M02/T01/pgm1.java
@@ -0,0 +1,22 @@
+
+public class pgm1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = scan.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.printlkn        }
+
+    }
+}
